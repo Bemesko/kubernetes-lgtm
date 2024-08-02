d3012ec4271e8b585d83cd77cef18ed443a843fa
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2024-08-02 14:30:53 -0300

    Change nodepool to standard b2asv2

--- main.tf
+++ main.tf
@@ -13,9 +13,10 @@ resource "azurerm_kubernetes_cluster" "monitoring" {
   resource_group_name = azurerm_resource_group.monitoring.name
   dns_prefix          = "aksecoshare"
   default_node_pool {
-    name       = "npsysdefault"
-    node_count = 1
-    vm_size    = "Standard_B2s_v2"
+    name                        = "npsysdefault"
+    temporary_name_for_rotation = "npsystemp"
+    node_count                  = 1
+    vm_size                     = "Standard_B2as_v2"
     upgrade_settings {
       drain_timeout_in_minutes      = 0
       max_surge                     = "10%"
